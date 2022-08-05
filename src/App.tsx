aa209d81a8f29b5c46f0f44d1b554c1ab3d9be63
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-08-05 15:37:54 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,69 @@ import { useState } from 'react';
 const AppWrapper = styled.div`
   width: 98%;
   max-width: 700px;
-  margin: 100 auto;
+  margin: 100px auto;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const Heading = styled.h2`
   font-size: 25px;
 `;
 
-const Count = styled.div`
-  background: purple;
+const Output = styled.div`
+  
 `;
 
 const ButtonWrapper = styled.div`
-  background: gray;
+  display: flex;
+  justify-content: center;
+  gap: 12px;
 `;
 
 const Button = styled.button`
   background: green;
   color: white;
+  padding: 8px 12px;
+  border: 0;
+  border-radius: 8px;
+`
+
+const FieldWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
 `
 
 
+ 
 function App() {
+  const [amount, setAmount] = useState(1)
+  const [count, setCount] = useState(amount)
+
+  let decrement = () => setCount(count => count - amount);
+  let increment = () => setCount(count => count + amount);
+  let reset = () => setCount(0);
+
+  let updateAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(event.target.value));
+  }
+
   return (
     <AppWrapper>
       <Heading>Counter with inputs, Typescript, SC & Cypress</Heading>
-      <Count></Count>
+      <Output>{count}</Output>
       <ButtonWrapper>
-        <Button>-</Button>
-        <Button>+</Button>
-        <Button>reset</Button>
+        <Button onClick={decrement}>-</Button>
+        <Button onClick={increment}>+</Button>
+        <Button onClick={reset}>reset</Button>
       </ButtonWrapper>
+      
+      <FieldWrapper>
+        <label htmlFor="amount">amount:</label>
+        <input type="text" name="amount" value={amount} onChange={updateAmount}/>
+      </FieldWrapper>
     </AppWrapper>
   );
 }

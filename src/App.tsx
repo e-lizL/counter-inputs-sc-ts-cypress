b043bb76fdcd23f0172375251fdae81d88c97bb2
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-08-05 13:06:37 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const AppWrapper = styled.div`
+  width: 98%;
+  max-width: 700px;
+  margin: 100 auto;
+`;
+
+const Heading = styled.h2`
+  font-size: 25px;
+`;
+
+const Count = styled.div`
+  background: purple;
+`;
+
+const ButtonWrapper = styled.div`
+  background: gray;
+`;
+
+const Button = styled.button`
+  background: green;
+  color: white;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Heading>Counter with inputs, Typescript, SC & Cypress</Heading>
+      <Count></Count>
+      <ButtonWrapper>
+        <Button>-</Button>
+        <Button>+</Button>
+        <Button>reset</Button>
+      </ButtonWrapper>
+    </AppWrapper>
   );
 }
 

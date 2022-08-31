7cbb60bc6cf7901da28966a4313220dac932b2c6
Elizabeth Lands <elizabeth_lands@hotmail.com>
2022-08-31 14:24:25 +0100

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import styled from 'styled-components';
 import { useState } from 'react';
 
 const AppWrapper = styled.div`
-  width: 98%;
-  max-width: 700px;
+  width: 95%;
+  max-width: 650px;
   margin: 100px auto;
   text-align: center;
   display: flex;
   flex-direction: column;
   gap: 20px;
+  background: #f47736;
+  color: #E6D5F4;
+  font-size: 40px;
 `;
 
 const Heading = styled.h2`
-  font-size: 25px;
+  font-size: 40px;
 `;
 
 const Output = styled.div`
+font-size: 40px;
   
 `;
 
@@ -23,20 +27,36 @@ const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   gap: 12px;
+  background: #FD9965;
+  padding: 20px;
 `;
 
 const Button = styled.button`
-  background: green;
+  background: #7425B2;
   color: white;
-  padding: 8px 12px;
+  padding: 20px 25px;
   border: 0;
-  border-radius: 8px;
+  border-radius: 5px;
+  font-size: 18px;
+  &:hover, &:active, &:focus {
+    background: #959C00;
+  }
 `
 
 const FieldWrapper = styled.div`
   display: flex;
   justify-content: center;
-  gap: 8px;
+  gap: 12px;
+  background: #FD9965;
+  padding: 20px;
+  margin-top: -20px;
+  color: white;
+`
+const Input = styled.input`
+  color: #7425B2;
+  border: 0;
+  border-radius: 5px;
+  font-size: 20px;
 `
 
 
@@ -64,8 +84,8 @@ function App() {
       </ButtonWrapper>
       
       <FieldWrapper>
-        <label htmlFor="amount">amount:</label>
-        <input type="text" name="amount" value={amount} onChange={updateAmount} data-cy="input"/>
+        <label htmlFor="amount">Amount:</label>
+        <Input type="text" name="amount" value={amount} onChange={updateAmount} data-cy="input"/>
       </FieldWrapper>
     </AppWrapper>
   );
